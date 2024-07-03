9c12d01aabf3dcb27475bf5ad561da8f5cbfa70c
Muhammad Irfan Fadilah <irfan.fadilah@inhealth.co.id>
2024-07-03 16:20:52 +0700

    add session on homepage

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,4 +1,4 @@
-import { getSession, useSession } from "next-auth/react";
+import { getSession } from "next-auth/react";
 import prisma from "../../../lib/prisma";
 
 // POST /api/post
@@ -6,17 +6,13 @@ import prisma from "../../../lib/prisma";
 // Optional fields in body: content
 export default async function handle(req, res) {
   const { title, content } = req.body;
-  const { data: session } = useSession();
 
-  console.log("session", session);
-
+  const session = await getSession({ req });
   const result = await prisma.post.create({
     data: {
       title: title,
       content: content,
-      author: {
-        connect: { email: session?.user?.email },
-      },
+      author: { connect: { email: session?.user?.email } },
     },
   });
   res.json(result);
